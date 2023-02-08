1eba963942de5fdec25d435dcef7b043388a925f
DreCampbell <dillon-campbell@hotmail.com>
2023-02-08 18:21:23 +0800

    Made changes to js file

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+document.querySelector('botton').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const playerName = document.querySelector('input').value
+    try{
+        const res = await fetch(`https://enchanting-seal-nightgown.cyclic.app/api/${playerName}`)
+        const data = await res.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.Name
+    }catch(error){
+        console.log(error)
+    }
+}
