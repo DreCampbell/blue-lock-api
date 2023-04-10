48dec648dbfcb4a6094898c14fa5080f6a97741c
DreCampbell <dillon-campbell@hotmail.com>
2023-04-10 16:21:18 +0800

    Testing

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,4 @@
-const url = 
-
 document.querySelector('button').addEventListener('click', apiRequest)
-
-document.querySelector('#isagi').addEventListener('click', isagi)
-document.querySelector('#bachira').addEventListener('click', bachira)
-document.querySelector('#kunigami').addEventListener('click', kunigami)
-document.querySelector('#chigiri').addEventListener('click', chigiri)
-document.querySelector('#gagamaru').addEventListener('click', gagamaru)
-document.querySelector('#imamura').addEventListener('click', imamura)
-document.querySelector('#naruhaya').addEventListener('click', naruhaya)
-document.querySelector('#kuon').addEventListener('click', kuon)
-document.querySelector('#igarashi').addEventListener('click', igarashi)
-document.querySelector('#raichi').addEventListener('click', raichi)
-document.querySelector('#iemon').addEventListener('click', iemon)
 
 async function apiRequest(){
     const playerName = document.querySelector('input').value
