73d701bbebd121de12de92cf2f85bc9438c2fc49
DreCampbell <dillon-campbell@hotmail.com>
2023-04-10 16:17:04 +0800

    Testing

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
-// const url = 
+const url = 
 
-// document.querySelector('button').addEventListener('click', apiRequest)
+document.querySelector('button').addEventListener('click', apiRequest)
 
 document.querySelector('#isagi').addEventListener('click', isagi)
 document.querySelector('#bachira').addEventListener('click', bachira)
@@ -14,25 +14,21 @@ document.querySelector('#igarashi').addEventListener('click', igarashi)
 document.querySelector('#raichi').addEventListener('click', raichi)
 document.querySelector('#iemon').addEventListener('click', iemon)
 
-function isagi(){
-    document.querySelector('.name').innerText = 'Isagi'
+async function apiRequest(){
+    const playerName = document.querySelector('input').value
+    try{
+        const res = await fetch(`https://bluelock-api.cyclic.app/api/${playerName}`)
+        const data = await res.json()
+
+        console.log(data)
+        document.querySelector('#playerCard').src = data.img
+        document.querySelector('#name').innerText = `Name: ${data.Name}`
+        document.querySelector('#alias').innerText = `Alias: ${data.Alias}`
+        document.querySelector('#age').innerText = `Age: ${data.Age}`
+        document.querySelector('#height').innerText = `Height: ${data.Height}`
+        document.querySelector('#weapon').innerText = `Weapon: ${data.Weapon}`
+        document.querySelector('#team').innerText = `Team: ${data.Team}`
+    }catch(error){
+        console.log(error)
+    }
 }
-
-// async function apiRequest(){
-//     const playerName = document.querySelector('input').value
-//     try{
-//         const res = await fetch(`https://bluelock-api.cyclic.app/api/${playerName}`)
-//         const data = await res.json()
-
-//         console.log(data)
-//         document.querySelector('#playerCard').src = data.img
-//         document.querySelector('#name').innerText = `Name: ${data.Name}`
-//         document.querySelector('#alias').innerText = `Alias: ${data.Alias}`
-//         document.querySelector('#age').innerText = `Age: ${data.Age}`
-//         document.querySelector('#height').innerText = `Height: ${data.Height}`
-//         document.querySelector('#weapon').innerText = `Weapon: ${data.Weapon}`
-//         document.querySelector('#team').innerText = `Team: ${data.Team}`
-//     }catch(error){
-//         console.log(error)
-//     }
-// }
