43168a7aa956ecdb894d9c1f0ce2f8922a91c34b
DreCampbell <dillon-campbell@hotmail.com>
2023-04-10 16:38:17 +0800

    Testing

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors())
 
 const players = {
     'isagi':{
-        'img': 'https://static.wikia.nocookie.net/bluelock/images/f/f3/Yoichi_Isagi_uniform_anime_design.png/revision/latest?cb=20220513125337',
+        'img': 'https://static.wikia.nocookie.net/bluelock/images/f/f3/Yoichi_Isagi_uniform_anime_design.png',
         'Name': 'Yoichi Isagi',
         'Position': 'Forward',
         'Age': 16,
